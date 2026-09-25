87d3604cfa83a29379e53ce8c3b29aa3250170c4
Tran Van Cong <congtv.bhsoft@gmail.com>
2026-09-25 22:05:23 +0700

    Bài 12: chuyển nghiệp vụ đơn hàng sang OOP

---
+++ bai12/product.php
@@ -0,0 +1,27 @@
+<?php
+class Product
+{
+    private string $name;
+    private float $price;
+    private int $quantity;
+
+    public function __construct($name, $price, $quantity)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
